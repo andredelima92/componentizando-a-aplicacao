e84a4b9fdcb5b3238571a6df6e19b60c291ff102
Andre <andredelima92@gmail.com>
2022-02-24 22:08:41 -0300

    clear challange

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+import "./../styles/sidebar.scss";
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+};
+
+export function SideBar({
+  genres,
+  setSelectedGenreId,
+  selectedGenreId,
+}: SideBarProps) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
